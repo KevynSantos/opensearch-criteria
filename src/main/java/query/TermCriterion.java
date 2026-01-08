ae9044ef691e1b731216d611d13bee746146daef
KevynSantos <52923238+KevynSantos@users.noreply.github.com>
2026-01-08 12:22:48 -0300

    Adicionando boost

--- src/main/java/query/TermCriterion.java
+++ src/main/java/query/TermCriterion.java
@@ -1,13 +1,5 @@
 package query;
 
-/**
-*
-*
-* @author Kevyn
-* @since 2026
-* @github https://github.com/KevynSantos
-*/
-
 import criteria.OsCriterion;
 import java.util.*;
 
@@ -16,16 +8,50 @@ public class TermCriterion implements OsCriterion {
     private final String field;
     private final Object value;
 
+    private Double boost;
+    private Boolean caseInsensitive;
+
     public TermCriterion(String field, Object value) {
         this.field = field;
         this.value = value;
     }
 
+    public TermCriterion boost(double boost) {
+        this.boost = boost;
+        return this;
+    }
+
+    public TermCriterion caseInsensitive(boolean value) {
+        this.caseInsensitive = value;
+        return this;
+    }
+
     @Override
     public Map<String, Object> toDsl() {
+
+        // forma simples
+        if (boost == null && caseInsensitive == null) {
+            return Collections.singletonMap(
+                "term",
+                Collections.singletonMap(field, value)
+            );
+        }
+
+        // forma expandida
+        Map<String, Object> options = new LinkedHashMap<>();
+        options.put("value", value);
+
+        if (boost != null) {
+            options.put("boost", boost);
+        }
+
+        if (caseInsensitive != null) {
+            options.put("case_insensitive", caseInsensitive);
+        }
+
         return Collections.singletonMap(
             "term",
-            Collections.singletonMap(field, value)
+            Collections.singletonMap(field, options)
         );
     }
 }
