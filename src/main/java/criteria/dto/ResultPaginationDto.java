40f5c3b2d05616f4833adc99e012e4c12d204109
KevynSantos <52923238+KevynSantos@users.noreply.github.com>
2026-01-14 16:19:37 -0300

    Tornando DTO imutavel

--- src/main/java/criteria/dto/ResultPaginationDto.java
+++ src/main/java/criteria/dto/ResultPaginationDto.java
@@ -1,65 +1,31 @@
 package criteria.dto;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ResultPaginationDto {
-	
-	public ResultPaginationDto(String responseBody, Integer pageSize, Integer page) throws JsonMappingException, JsonProcessingException
-	{
-		ObjectMapper mapper = new ObjectMapper();
-		JsonNode root = mapper.readTree(responseBody);
-		int total = root.path("hits").path("total").path("value").asInt();
-		JsonNode records = root.path("hits").path("hits");
-		
-		this.page = page;
-		this.pageSize = pageSize;
-		this.records = records;
-		this.total = total;
-	}
-	
-	private Integer page;
-	
-	private Integer pageSize;
-	
-	private Integer total;
-	
-	private JsonNode records;
 
-	public Integer getPage() {
-		return page;
-	}
+    private final Integer page;
+    private final Integer pageSize;
+    private final Integer total;
+    private final JsonNode records;
 
-	public void setPage(Integer page) {
-		this.page = page;
-	}
+    public ResultPaginationDto(String responseBody, Integer pageSize, Integer page)
+            throws JsonProcessingException {
 
-	public Integer getPageSize() {
-		return pageSize;
-	}
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root = mapper.readTree(responseBody);
 
-	public void setPageSize(Integer pageSize) {
-		this.pageSize = pageSize;
-	}
+        this.total = root.path("hits").path("total").path("value").asInt();
+        this.records = root.path("hits").path("hits");
+        this.page = page;
+        this.pageSize = pageSize;
+    }
 
-	public Integer getTotal() {
-		return total;
-	}
-
-	public void setTotal(Integer total) {
-		this.total = total;
-	}
-
-	public JsonNode getRecords() {
-		return records;
-	}
-
-	public void setRecords(JsonNode records) {
-		this.records = records;
-	}
-	
-	
-
+    public Integer getPage() { return page; }
+    public Integer getPageSize() { return pageSize; }
+    public Integer getTotal() { return total; }
+    public JsonNode getRecords() { return records; }
 }
+
